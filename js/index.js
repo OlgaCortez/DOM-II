6ab4e86071996853e93cea49dfa1a473ea85fb47
Olga Cortez <olgalataviacortez@gmail.com>
2019-08-06 18:45:55 -0500

    3 events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
 // Your code goes here
+
+//Transition on the img-content
+const transition = document.querySelector('.img-content');
+
+transition.addEventListener('transitionend', () => {
+  console.log('Transition ended');
+});
+
+//for <p> under h2: Welcome to fun bus
+document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+  log.textContent += ` ${e.code}`;
+}
+
+//h2 Welcome Fun Bus Header
+let test = document.getElementById("test");
+  
+test.addEventListener("mouseenter", function( event ) {   
+  event.target.style.color = "yellow";
+
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+test.addEventListener("mouseover", function( event ) {   
+  event.target.style.color = "orange";
+
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+
+
+
