dd33047a3a94cebbf7db852f2eba4056c8e3ef5d
Olga Cortez <olgalataviacortez@gmail.com>
2019-08-06 22:13:05 -0500

    update

--- js/index.js
+++ js/index.js
@@ -33,6 +33,52 @@ test.addEventListener("mouseover", function( event ) {
   }, 500);
 }, false);
 
+const destination = document.querySelector('.destination');
+
+destination.addEventListener('lostpointercapture', () => {
+  console.log('I\'ve been released!');
+});
+
+destination.addEventListener('pointerdown', (event) => {
+  destination.setPointerCapture(event.pointerId);
+  console.log('Pointer down event');
+});
+
+//If transition of pics are cancelled
+const trans = document.querySelector('.img-content');
+
+trans.addEventListener('transitioncancel', () => {
+  console.log('Transition canceled');
+});
+
+//Before printing
+window.addEventListener('beforeprint', (event) => {
+    console.log('Before print');
+  });
+
+//After printing
+  window.addEventListener('afterprint', (event) => {
+    console.log('After print');
+  });
+
+ //Width Changing
+  const width = document.querySelector('width');
+  function reportWindowSize() {
+    console.log('Width changed.');
+  }
+  
+  window.onresize = reportWindowSize;  
+
+//when you double click on the fun-bus pic it gets larger.
+  const card = document.querySelector('aside');
+
+  card.addEventListener('dblclick', function (e) {
+    card.classList.toggle('large');
+  });
 
 
+  const button = document.querySelector('button');
 
+  button.addEventListener('click', event => {
+    button.innerHTML = `Click count: ${event.detail}`;
+  });
